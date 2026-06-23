586ed367f8af2058fa48261001b9aabae0ddd2f2
gzwcy63 <58155086@qq.com>
2026-06-23 18:55:40 +0800

    add railway vars

---
+++ server/railway_vars.py
@@ -0,0 +1,6 @@
+# Railway environment variables (pushed to repo)
+# These override system env vars for production
+RAILWAY_BAIDU_OCR_APP_ID = "7870788"
+RAILWAY_BAIDU_OCR_API_KEY = "aia6IlbH25S5T9B5w7wvbyw8"
+RAILWAY_BAIDU_OCR_SECRET_KEY = "o6rJf4CUBasyhjkqFtdEwPvbglodfvxT"
+RAILWAY_DEEPSEEK_API_KEY = "o6rJf4CUBasyhjkqFtdEwPvbglodfvxT"
